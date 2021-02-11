5fea7017f1318b0041c15950f8c56c1718db0be6
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2021-02-11 22:34:59 +0200

    feat: initial example code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,102 @@
-import "./styles.css";
-
+import styles from "./App.module.css";
+/*
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}*/
+
+import React from "react";
+import { EmptyStarIcon, FilledStarIcon } from "./components/icons";
+//import styles from "../styles/Home.module.css";
+
+import { gql, useQuery, useMutation } from "@apollo/client";
+
+const GET_REPOSITORY = gql`
+  query RepositoryQuery($repository: String!, $owner: String!) {
+    repository(name: $repository, owner: $owner) {
+      __typename
+      id
+      name
+      description
+      forkCount
+      stargazerCount
+    }
+  }
+`;
+
+const ADD_STAR = gql`
+  mutation AddStarMutation($starrable: AddStarInput!) {
+    addStar(input: $starrable) {
+      clientMutationId
+      starrable {
+        id
+        stargazerCount
+        __typename
+      }
+    }
+  }
+`;
+
+const REMOVE_STAR = gql`
+  mutation RemoveStarMutation($starrable: RemoveStarInput!) {
+    removeStar(input: $starrable) {
+      clientMutationId
+      starrable {
+        id
+        stargazerCount
+        __typename
+      }
+    }
+  }
+`;
+
+export default function App() {
+  const [starred, setStarred] = React.useState(false);
+
+  const { loading, error, data: queryData } = useQuery(GET_REPOSITORY, {
+    variables: {
+      owner: "juhanakristian",
+      repository: "next-graphql-msw-example"
+    }
+  });
+
+  const [addStar] = useMutation(ADD_STAR);
+  const [removeStar] = useMutation(REMOVE_STAR);
+
+  function handleClickStar(e) {
+    e.preventDefault();
+    if (starred) {
+      addStar({ starrable: { starrableId: queryData.repository.id } });
+    } else {
+      removeStar({ starrable: { starrableId: queryData.repository.id } });
+    }
+
+    setStarred(!starred);
+  }
+
+  return (
+    <div className={styles.container}>
+      {loading || !queryData ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          <h3>{queryData.repository.name}</h3>
+          <p>{queryData.repository.description}</p>
+          <div className={styles.star}>
+            <button onClick={handleClickStar}>
+              {starred ? <FilledStarIcon /> : <EmptyStarIcon />}
+              <span>{starred ? "Unstar" : "Star"}</span>
+            </button>
+            <a href="https://github.com/juhanakristian/next-graphql-msw-example/stargazers">
+              {queryData.repository.stargazerCount}
+            </a>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://api.github.com/graphql",
+  cache: new InMemoryCache()
+});
+
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>,
   rootElement
 );
